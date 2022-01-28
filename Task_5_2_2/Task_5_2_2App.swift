ec50f59701f33b57c6ca852a6ea1b37408169545
Legenda759 <oripov1994@gmail.com>
2022-01-28 23:07:34 +0500

    Initial Commit

---
+++ Task_5_2_2/Task_5_2_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_5_2_2App.swift
+//  Task_5_2_2
+//
+//  Created by Legenda_759 on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Task_5_2_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
